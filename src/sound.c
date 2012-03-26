819bede9b80860c49eb7666c163bfe42f21dd2c6
Marisa-Chan <llancelot7@gmail.com>
2012-03-26 09:10:41 +0700

    Fix to SDL_mixer

--- src/sound.c
+++ src/sound.c
@@ -18,52 +18,56 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include <mikmod.h>
+
 #include <pthread.h>
 #include <glib.h>
 
 #include "sound.h"
 #include "board_engine.h"
+#include <SDL/SDL.h>
+#include <SDL/SDL_mixer.h>
 
 static pthread_t thread;
 
-static MODULE *module;
-static SAMPLE *swap_sfx = NULL;
-static SAMPLE *click_sfx = NULL;
+static Mix_Music *module;
+static Mix_Chunk *swap_sfx = NULL;
+static Mix_Chunk *click_sfx = NULL;
 
 static gboolean is_playing;
 static gboolean sound_available;
 
-void sound_thread(void *ptr)
-{
-	while (1) {
-	    g_usleep(10000);
-		MikMod_Update();
-    }
-}
+static int audio_rate = 44100;
+static uint16_t audio_format = MIX_DEFAULT_FORMAT; /* 16-bit stereo */
+static int audio_channels = MIX_DEFAULT_CHANNELS;
+static int audio_buffers = 1024;
+
 
 void sound_init()
 {
+    SDL_Init(SDL_INIT_AUDIO);
+    Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers);
+    sound_available = TRUE;
+
     /* register all the drivers */
-    MikMod_RegisterDriver(&drv_esd);
-    MikMod_RegisterDriver(&drv_alsa);
-    MikMod_RegisterDriver(&drv_oss);
-    MikMod_RegisterDriver(&drv_nos);
+    //MikMod_RegisterDriver(&drv_esd);
+   // MikMod_RegisterDriver(&drv_alsa);
+    //MikMod_RegisterDriver(&drv_oss);
+   // MikMod_RegisterDriver(&drv_nos);
 
     /* register only the s3m module loader */
-    MikMod_RegisterLoader(&load_s3m);
+   // MikMod_RegisterLoader(&load_s3m);
 
     /* initialize the library */
-    if (MikMod_Init("")) {
-        g_printerr("Could not initialize sound, reason: %s\n", MikMod_strerror(MikMod_errno));
-        MikMod_Exit();
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
         sound_available = FALSE;
     } else {
-        sound_available = TRUE;
-        MikMod_EnableOutput();
-        pthread_create(&thread, NULL, (void *)&sound_thread, NULL);
-        g_print("Audio driver choosen: %s\n", md_driver->Name);
+        if (Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) >= 0)
+            sound_available = TRUE;
+        else
+            sound_available = FALSE;
     }
+    if (!sound_available)
+        g_printerr("Could not initialize sound, reason: %s\n", Mix_GetError());
 
     is_playing = FALSE;
 }
@@ -72,15 +76,14 @@ void sound_init()
 void sound_music_play()
 {
 	if (!is_playing && sound_available) {
-	    // load module
-	    module = Player_Load(DATADIR "/sounds/gweled/autonom.s3m", 64, 0);
+	    module = Mix_LoadMUS(DATADIR "/sounds/gweled/autonom.s3m");
     	if (module) {
-    	    Player_Start(module);
-			Player_SetVolume(64);
+
+    	    Mix_PlayMusic(module,-1);
 			is_playing = TRUE;
 
     	} else
-    	    g_printerr("Could not load module, reason: %s\n", MikMod_strerror(MikMod_errno));
+    	    g_printerr("Could not load module, reason: %s\n", Mix_GetError());
 	}
 }
 
@@ -89,8 +92,10 @@ void sound_music_stop()
 	if (is_playing && sound_available) {
     	if (module)
 		{
-	    	Player_Stop();
-	    	Player_Free(module);
+		    Mix_HaltMusic();
+		    Mix_FreeMusic(module);
+	    	//Player_Stop();
+	    	//Player_Free(module);
 	    	is_playing = FALSE;
 		}
 	}
@@ -103,26 +108,25 @@ void sound_load_samples()
         return;
 
     if (!swap_sfx)
-        swap_sfx = Sample_Load(DATADIR "/sounds/gweled/swap.wav");
+        swap_sfx = Mix_LoadWAV(DATADIR "/sounds/gweled/swap.wav");
     if (!swap_sfx)
-        g_warning("Could not load swap.wav, reason: %s", MikMod_strerror(MikMod_errno));
+        g_warning("Could not load swap.wav, reason: %s", Mix_GetError());
 
     if (!click_sfx)
-        click_sfx = Sample_Load(DATADIR "/sounds/gweled/click.wav");
+        click_sfx = Mix_LoadWAV(DATADIR "/sounds/gweled/click.wav");
     if (!click_sfx)
-        g_warning("Could not load click.wav, reason: %s", MikMod_strerror(MikMod_errno));
+        g_warning("Could not load click.wav, reason: %s", Mix_GetError());
 
-    MikMod_SetNumVoices(-1, 2);
 }
 
 void sound_unload_samples()
 {
     if (swap_sfx) {
-		Sample_Free(swap_sfx);
+		Mix_FreeChunk(swap_sfx);
 		swap_sfx = NULL;
 	}
 	if (click_sfx) {
-		Sample_Free(click_sfx);
+		Mix_FreeChunk(click_sfx);
 		click_sfx = NULL;
 	}
 }
@@ -136,11 +140,11 @@ void sound_play_sample(gweled_sound_samples sample)
     switch (sample) {
         case CLICK_EVENT:
             if(click_sfx)
-                Sample_Play(click_sfx, 0, 0);
+                Mix_PlayChannel(-1,click_sfx, 0);
             break;
         case SWAP_EVENT:
             if(swap_sfx)
-                Sample_Play(swap_sfx, 0, 0);
+                Mix_PlayChannel(-1,swap_sfx, 0);
             break;
     }
 }
@@ -150,13 +154,9 @@ void sound_destroy()
     if (sound_available) {
         sound_music_stop();
 
-        pthread_cancel(thread);
-        pthread_join(thread, NULL);
-
         sound_unload_samples();
 
-        MikMod_DisableOutput();
-	    MikMod_Exit();
+        Mix_CloseAudio();
 
 	    sound_available = FALSE;
 	}
